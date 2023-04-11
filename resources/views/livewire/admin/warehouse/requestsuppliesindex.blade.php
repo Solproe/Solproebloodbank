4effde5ebf3f5e5b93f110ae8399d5ffdeb9ff6b
Solproe <iperez_fragozo@live.com>
2023-04-11 09:20:35 -0500

    duff

--- resources/views/livewire/admin/warehouse/requestsuppliesindex.blade.php
+++ resources/views/livewire/admin/warehouse/requestsuppliesindex.blade.php
@@ -7,7 +7,9 @@
             <div class="row">
                 <div class="col">
                     <input class="form-check-input" type="checkbox" value="{{ $supply->listado }}" id="options" />
-                    <label class="form-check-label" for="options">{{ $supply->supply_name }}</label>
+                    <label class="form-check-label"
+                        style="font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif"
+                        for="options">{{ $supply->supply_name }}</label>
                 </div>
                 <div class="col">
                     <input type="text">
