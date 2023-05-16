d86944a32ce7552f325d01bea8d5c9f2df0127ba
luisdanielcoronelposada903@gmail.com <luisdanielcoronelposada903@gmail.com>
2023-05-16 11:25:19 -0500

    merge v20

--- app/Http/Controllers/APIs/requestapi/RequestController.php
+++ app/Http/Controllers/APIs/requestapi/RequestController.php
@@ -43,7 +43,8 @@ class RequestController extends Controller
         {
             try
             {
-                $validateReceived->update(['news' => json_encode($request->annotation)]);
+                $validateReceived->update(['news' => json_encode($request->annotation),
+                                            'received_date' => $date]);
 
                 return true;
             }
