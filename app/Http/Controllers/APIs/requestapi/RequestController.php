a9054be224379d61d8f0b44a3f106cb8c04c83ae
luisdanielcoronelposada903@gmail.com <luisdanielcoronelposada903@gmail.com>
2023-05-16 10:39:41 -0500

    merge v14

--- app/Http/Controllers/APIs/requestapi/RequestController.php
+++ app/Http/Controllers/APIs/requestapi/RequestController.php
@@ -26,7 +26,7 @@ class RequestController extends Controller
     {
         $validateReceived = ValidateReceivedModel::where('consecutive', $id)->first();
 
-        $validateReceived->update(['news' => json_encode($request->status)]);
+        $validateReceived->update(['news' => json_encode($request->annotation)]);
 
         $status = status::where('status_name', $request->status)->first();
 
