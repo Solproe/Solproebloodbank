8f2dda099ef4f072dd13877173350e7d1609c8e9
luisdanielcoronelposada903@gmail.com <luisdanielcoronelposada903@gmail.com>
2023-06-21 11:04:29 -0500

    duss

--- app/Services/FirebaseRealTimeDatabase.php
+++ app/Services/FirebaseRealTimeDatabase.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Http\Middleware\data;
 use App\Models\Center;
+use App\Models\delivery;
 use Exception;
 use Kreait\Firebase\Database\Snapshot as DatabaseSnapshot;
 use Kreait\Firebase\Factory;
@@ -47,11 +48,13 @@ class FirebaseRealTimeDatabase
         $date = substr($data->date, 0, 4) . "/" . substr($data->date, 5, 2) . "/" .
         substr($data->date, 8, 2) . " " . substr($data->date, 11, 5);
 
+        $delivery = delivery::where("id_delivery", $data->through)->first();
+
         $json = [
             'consecutive' => $data->consecutive,
             'unities' => $data->unities,
             'boxes' => $data->boxes,
-            'through' => $data->through,
+            'through' => $delivery->des_delivery,
             'status' => $data->status->status_name,
             'date' => $date,
         ];
