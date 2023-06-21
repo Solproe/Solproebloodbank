14fec4e617408f989101732e8129b8bca817236a
luisdanielcoronelposada903@gmail.com <luisdanielcoronelposada903@gmail.com>
2023-06-21 10:42:10 -0500

    duss

--- app/Services/FirebaseRealTimeDatabase.php
+++ app/Services/FirebaseRealTimeDatabase.php
@@ -47,8 +47,6 @@ class FirebaseRealTimeDatabase
         $date = substr($data->date, 0, 4) . "/" . substr($data->date, 5, 2) . "/" .
         substr($data->date, 8, 2) . " " . substr($data->date, 11, 5);
 
-        dd($date);
-
         $json = [
             'consecutive' => $data->consecutive,
             'unities' => $data->unities,
