322dd3f50ca09388ee6831fc8e36ccd735636bb7
luisdanielcoronelposada903@gmail.com <luisdanielcoronelposada903@gmail.com>
2023-06-27 17:23:06 -0500

    new API route

---
+++ app/Http/Controllers/Admin/appUsers/AppUsersController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Admin\appUsers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AppUsersController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function update(Request $request, $id)
+    {
+
+    }
+
+    public function destroy($id)
+    {
+
+    }
+}
