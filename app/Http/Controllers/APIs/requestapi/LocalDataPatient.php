46ece5199be9acc13bd86b7f7c792342c6915eaf
domainKT01 <luisdanielcoronelposada903@gmail.com>
2023-09-07 12:53:03 -0500

    api local data

---
+++ app/Http/Controllers/APIs/requestapi/LocalDataPatient.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\APIs\requestapi;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LocalDataPatient extends Controller
+{
+    //
+}
