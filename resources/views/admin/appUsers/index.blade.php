77f7439d58447bc745b31b1894ac4a11bf0f3142
domainKT01 <luisdanielcoronelposada903@gmail.com>
2023-11-04 22:42:10 -0500

    rebuild database structure v1.3

--- resources/views/admin/appUsers/index.blade.php
+++ resources/views/admin/appUsers/index.blade.php
@@ -53,7 +53,7 @@
                                                                 <option selected>Select BloodBank</option>
                                                                 @foreach ($centers as $center)
                                                                     <option value="{{ $center->ID_CENTRE }}">
-                                                                        {{ $center->COD_CENTRE }}</option>
+                                                                        {{ $center->DES_CENTRE }}</option>
                                                                 @endforeach
                                                             </select>
                                                         </td>
