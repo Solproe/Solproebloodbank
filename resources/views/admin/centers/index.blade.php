f51e035f17e6372bf4b5664f1d315047b91a7dfa
domainKT01 <luisdanielcoronelposada903@gmail.com>
2023-11-14 16:16:15 -0500

    data structure log v1.5

--- resources/views/admin/centers/index.blade.php
+++ resources/views/admin/centers/index.blade.php
@@ -189,25 +189,25 @@
                                                             </select>
                                                         </div>
                                                         <div class="input-group mb-3">
-                                                            <input type="text" id="ip"
+                                                            <input type="text" id="ip" name="PUBLIC_IP"
                                                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
                                                              focus:ring-blue-500 focus:border-blue-500 block w-24 min-w-full
                                                              dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
                                                              dark:focus:ring-blue-500 dark:focus:border-blue-500 mr-2 form-control"
                                                                 placeholder="Public IP">
-                                                            <input type="text" id="db_name"
+                                                            <input type="text" id="db_name" name="DB_NAME"
                                                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
                                                              focus:ring-blue-500 focus:border-blue-500 block w-48
                                                              dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
                                                              dark:focus:ring-blue-500 dark:focus:border-blue-500 mr-2 form-control"
                                                                 placeholder="Db name">
-                                                            <input type="text" id="db_user"
+                                                            <input type="text" id="db_user" name="DB_USER"
                                                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
                                                              focus:ring-blue-500 focus:border-blue-500 block w-24 min-w-full
                                                              dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
                                                              dark:focus:ring-blue-500 dark:focus:border-blue-500 mr-2 form-control"
                                                                 placeholder="Db user">
-                                                            <input type="text" id="passwd"
+                                                            <input type="text" id="passwd" name="PASSWD"
                                                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
                                                              focus:ring-blue-500 focus:border-blue-500 block w-24 min-w-full
                                                              dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
